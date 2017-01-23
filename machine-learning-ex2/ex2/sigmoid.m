c4b5d42d843dc192a0ca84dbb635f228820552fb
Thanh C. Tran <trancong@rhrk.uni-kl.de>
2017-01-23 21:16:28 +0100

    Implement sigmoid function
    # Explain what has been changed
    
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

--- machine-learning-ex2/ex2/sigmoid.m
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -8,8 +8,8 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
-
-
+ g = 1 ./ (1 + exp(-z));
+% g = sigmf(z,[1 0]); % if licensed Fuzzy Logic Toolbox
 
 
 
