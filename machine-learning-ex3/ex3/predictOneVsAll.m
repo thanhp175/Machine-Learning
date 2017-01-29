72df6cbf04034eb689f2b9b59176b80f683b9c3c
Thanh C. Tran <trancong@rhrk.uni-kl.de>
2017-01-30 00:04:04 +0100

    Implement prediction for one-vs-all classification
    # Explain what has been changed
    
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

--- machine-learning-ex3/ex3/predictOneVsAll.m
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -29,11 +29,8 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-
-
-
-
-
+H = sigmoid(X*(all_theta'));
+[h, p] = max(H,[],2);
 
 
 % =========================================================================
