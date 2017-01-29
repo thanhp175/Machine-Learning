9596a28b56a3eb933f872e9af9cd002dec83d049
Thanh C. Tran <trancong@rhrk.uni-kl.de>
2017-01-30 00:36:38 +0100

    Implement neural network prediction
    # Explain what has been changed
    
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,13 +21,15 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
-
-
-
-
-
-
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+for i = 1:m
+    a1 = X(i,:);
+    a2 = sigmoid(Theta1*a1');
+    a2 = [1; a2];
+    a3 = sigmoid(Theta2*a2);
+    [h, p(i)] = max(a3);
+end
 
 % =========================================================================
 
